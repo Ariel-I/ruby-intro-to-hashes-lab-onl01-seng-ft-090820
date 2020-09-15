a79a05b7dcbaf3775731d62db412c4e9d51724e1
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 04:41:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,16 @@
 def new_hash
-
+  new_hash = {}
 end
 
 def actor
-
+  actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {:railroads =>{},
+	monopoly_with_second_tier
+	  
+	}
 
 end
 
